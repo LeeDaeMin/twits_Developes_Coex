06db1e2671e341a9406ee366d5eb0214ab1d5b1c
Jose Orejarena <jmoc951@gmail.com>
2022-10-07 15:58:34 -0500

    Configuracion deel feedController y del userController

--- app/Http/Controllers/feedController.php
+++ app/Http/Controllers/feedController.php
@@ -10,19 +10,127 @@ class feedController extends Controller
     
     public function create(Request $request){
 
-        $request -> validate([
 
-            'user_id' => 'required|integer'
+        try{
 
-        ]);
+            
+            $request -> validate([
+                
+                'user_id' => 'required|integer'
+                
+            ]);
+            
+            $feed = Feed::create([
+                
+                'user_id' => $request->user_id
+                
+            ]);
+        
+            return response() -> json($feed,200);
+    
+        }catch(\Exception $e){
 
-        $feed = Feed::create([
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
 
-            'user_id' => $request->user_id
+        }
 
-        ]);
+    }
 
-        return response() -> json($feed);
+    public function index(){
+
+        try{
+
+            $feeds = Feed::all();
+            
+            return response() -> json($feeds,200); 
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+    }
+
+    public function show($id){
+
+        try{
+
+            $feed = Feed::find($id);
+
+            return response() -> json($feed,200);
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+    }
+
+    public function update(Request $request, $id){
+
+        try{
+
+            $request = $request -> all();
+
+            $feed = Feed::find($id) ;
+
+            if(!$feed){
+
+                return 'Feed Not Found';
+
+            }else{
+
+                $feed -> update($request);
+
+            }
+            
+            return response() -> json($feed,200);
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+    }
+
+    public function delete($id){
+        
+        try{
+
+            $feed = Feed::find($id) ;
+
+            if(!$feed){
+
+                return 'Feed Not Found';
+
+            }else{
+
+                $feed -> destroy($id);
+                return $feed;
+
+            }
+            
+            return response() -> json($feed,200);
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
 
     }
 
--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -3,8 +3,139 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class userController extends Controller
 {
-    //
+    public function create(Request $request){
+
+
+        try{
+
+            
+            $request -> validate([
+                
+                'email' => 'required|integer',
+                'password' => 'required|integer',
+                'name' => 'required|string',
+                'phone' => 'required|string'
+                
+            ]);
+            
+            $user = User::create([
+                
+                'email' => $request->email,
+                'password' => $request->password,
+                'name' => $request->name,
+                'phone' => $request->phone
+                
+            ]);
+        
+            return response() -> json($user,200);
+    
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+    }
+
+    public function index(){
+
+        try{
+
+            $users = User::all();
+            
+            return response() -> json($users,200); 
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+    }
+
+    public function show($id){
+
+        try{
+
+            $user = User::find($id);
+
+            return response() -> json($user,200);
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+    }
+
+    public function update(Request $request, $id){
+
+        try{
+
+            $request = $request -> all();
+
+            $user = User::find($id) ;
+
+            if(!$user){
+
+                return 'Feed Not Found';
+
+            }else{
+
+                $user -> update($request);
+
+            }
+            
+            return response() -> json($user,200);
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+    }
+
+    public function delete($id){
+        
+        try{
+
+            $user = User::find($id) ;
+
+            if(!$user){
+
+                return 'Feed Not Found';
+
+            }else{
+
+                $user -> destroy($id);
+                return $user;
+
+            }
+            
+            return response() -> json($user,200);
+
+        }catch(\Exception $e){
+
+            return response() -> json([
+                'error' => $e -> getMessage()
+            ],404);
+
+        }
+
+
+    }
 }
