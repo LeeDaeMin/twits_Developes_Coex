78bba772f403074ee423d02ef7f65a4f47796a44
FenixThink <brayanxdvanegas@hotmail.com>
2022-10-07 17:58:05 -0500

    Upload Relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Comment;
+use App\Models\Feed;
+use App\Models\Tweet;
 
 class User extends Authenticatable
 {
@@ -41,4 +44,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    public function feed()
+    {
+        return $this->hasMany(Feed::class);
+    }
+    public function tweet()
+    {
+        return $this->hasMany(Tweet::class);
+    }
 }
+
+/* user -1> comentarios */
